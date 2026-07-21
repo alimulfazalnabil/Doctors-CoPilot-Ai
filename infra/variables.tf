62458558fce5e11e4e7561fe358b3eb8b6ecafbb
Alimul Fazal Nabil <43905688+alimulfazalnabil@users.noreply.github.com>
2026-07-21 20:32:23 +0600

    Add files via upload

---
+++ infra/variables.tf
@@ -0,0 +1,3 @@
+variable "resource_group_name" { type = string, default = "rg-doctors-copilot-prod" }
+variable "location" { type = string, default = "eastus2" }
+variable "environment" { type = string, default = "prod" }
